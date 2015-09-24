f7ed8ff4885ddd85a6934a647210c9c98c4b2738
haoyiguo <haoyiguo@10.84.8.34>
2015-09-24 00:45:49 -0400

    commit

--- Tutorial1/src/Maze.java
+++ Tutorial1/src/Maze.java
@@ -1,14 +1,35 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Maze {
 	
-	private String fileName;
+	private String map;
 	
-	public Maze(String file) {
-		fileName = file;
+	public Maze(String fileName) {
+		try
+		{
+			FileReader fin = new FileReader(fileName);
+			Scanner scan = new Scanner(fin);
+			while (scan.hasNext())
+			{
+				map = map + scan.next();
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("File Not Found");
+		}
+	}
+	
+	public String toString() {
+		return map;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Maze maz = new Maze("map01.txt");
+		System.out.println(maz);
 
 	}
 
